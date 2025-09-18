c4f251f40c83ef0c0fdfffe23c3603bebde34a92
ShewinVega <edwinvega.aid@gmail.com>
2025-09-18 14:38:41 -0600

    feat: patient and superadmin role name were added to the environment variables, roles(tutor and therapist) alias were added.

--- src/constants/roles.constants.js
+++ src/constants/roles.constants.js
@@ -1,7 +1,13 @@
 module.exports = {
+  // Roles
   ADMIN_ROLE: 'Admin',
   SUPERADMIN_ROLE: 'Superadmin',
   TUTOR_ROLE: 'Tutor',
   THERAPIST_ROLE: 'Terapeuta',
   PATIENT_ROLE: 'Paciente',
+
+  // Aliases
+  TUTOR_ALIAS: 'tutor',
+  THERAPIST_ALIAS: 'therapist',
+  PATIENT_ALIAS: 'patient',
 }
